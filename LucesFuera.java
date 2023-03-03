733f057e020f75ea5967cecd53a4aabea1303440
JuMiLuGa <lubian.ganete.juliomiguel.ald@gmail.com>
2023-03-03 17:29:18 +0100

    añadido metodo invertir posiciones

--- LucesFuera.java
+++ LucesFuera.java
@@ -37,7 +37,21 @@ public class LucesFuera {
   }
 
   public void invertirPosiciones(int x, int y) {
-
+    if (fila >= 0 && fila < tamaño && columna >= 0 && columna < tamaño) {
+      matriz[fila][columna] = !matriz[fila][columna];
+      if (fila > 0) {
+          matriz[fila - 1][columna] = !matriz[fila - 1][columna];
+      }
+      if (fila < tamaño - 1) {
+          matriz[fila + 1][columna] = !matriz[fila + 1][columna];
+      }
+      if (columna > 0) {
+          matriz[fila][columna - 1] = !matriz[fila][columna - 1];
+      }
+      if (columna < tamaño - 1) {
+          matriz[fila][columna + 1] = !matriz[fila][columna + 1];
+      }
+  } 
   }
 
 
